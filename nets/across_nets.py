8f6632383f3fc1bc60cd6f6633b455c82ed25389
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-05-26 21:38:23 -0700

    provenance commit 2016-05-26 21:38:18.903694.

--- nets/across_nets.py
+++ nets/across_nets.py
@@ -2,7 +2,7 @@
 #name the directory
 
 import caffe_net_response as cf
-ann_dir = '/home/dean/caffe/models/bvlc_reference_caffenet/'
+ann_dir = '/data/dean_data/net_stages/'
 import os
 import sys
 top_dir = os.getcwd().split('v4cnn')[0] 
@@ -29,15 +29,15 @@ stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(370
                                                              rotation = None)
 
 
-all_iter = dm.list_files(ann_dir + 'caffenet_train/_iter*.caffe*')
+all_iter = dm.list_files(ann_dir + '_iter*.caffe*')
 
 for fn in all_iter:
 
-    da = cf.get_net_resp(base_image_nm, ann_dir, fn.split('ce_caffenet/')[1].split('.')[0], 
+    da = cf.get_net_resp(base_image_nm, ann_dir, fn.split('stages/')[1].split('.')[0], 
                          stim_trans_cart_dict, stim_trans_dict, require_provenance=True)
     tn = int(fn.split('iter_')[1].split('.')[0])   
     da.attrs['train']= tn
     ds = da.to_dataset(name ='resp')
-    ds.to_netcdf(top_dir + 'data/responses/iter_' + str(tn) + '.nc')
+    ds.to_netcdf('/data/dean_data/responses/iter_' + str(tn) + '.nc')
 
 
