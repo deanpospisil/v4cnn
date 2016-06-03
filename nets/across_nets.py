6ccf1f2a160b649f9be41c185a2b29f51ee4fabe
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-06-03 14:57:12 -0700

    provenance commit 2016-06-03 14:57:12.043444.

--- nets/across_nets.py
+++ nets/across_nets.py
@@ -23,8 +23,8 @@ ann_fn = 'caffenet_train_iter_'
 stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(370),
                                                              blur=None,
                                                              scale =(0.45,0.45,1),
-                                                             x=(-7, 7, 15),
-#                                                             x=(-50, 50, 1),
+                                                             #x=(-7, 7, 15),
+                                                             x=(-50, 50, 1),
                                                              y=None,
                                                              rotation = None)
 
@@ -38,5 +38,5 @@ for fn in all_iter:
     tn = int(fn.split('iter_')[1].split('.')[0])   
     da.attrs['train']= tn
     ds = da.to_dataset(name ='resp')
-    ds.to_netcdf('/data/dean_data/responses/iter_small_trans' + str(tn) + '.nc')
+    #ds.to_netcdf('/data/dean_data/responses/iter_small_trans' + str(tn) + '.nc')
 
