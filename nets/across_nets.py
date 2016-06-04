53dc3263d34efed5c2dcd1d2d0120e6d7b913dc5
Dean Pospisil <dpospisi@students.pitzer.edu>
2016-06-03 17:28:01 -0700

    provenance commit 2016-06-03 17:28:01.180619.

--- nets/across_nets.py
+++ nets/across_nets.py
@@ -22,11 +22,19 @@ ann_fn = 'caffenet_train_iter_'
 #
 stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(370),
                                                              blur=None,
-                                                             scale =(0.45,0.45,1),
+                                                             scale =None,
                                                              x=(-7, 7, 15),
                                                              #x=(-50, 50, 1),
                                                              y=None,
                                                              rotation = None)
+                                                             #
+#stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(370),
+#                                                             blur=None,
+#                                                             scale =(0.45,0.45,1),
+#                                                             x=(-50, 48, 50),
+#                                                             #x=(-50, 50, 1),
+#                                                             y=None,
+#                                                             rotation = None)
 
 
 all_iter = dm.list_files(ann_dir + '_iter*.caffe*')
@@ -38,5 +46,5 @@ for fn in all_iter:
     tn = int(fn.split('iter_')[1].split('.')[0])   
     da.attrs['train']= tn
     ds = da.to_dataset(name ='resp')
-    ds.to_netcdf('/data/dean_data/responses/iter_small_trans' + str(tn) + '.nc')
+    ds.to_netcdf('/data/dean_data/responses/iter_large_few_trans' + str(tn) + '.nc')
 
