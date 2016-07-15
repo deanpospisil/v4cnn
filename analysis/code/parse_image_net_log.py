33f1ec9bb55f91f689d0b7b9f91aa5f98e42ad41
Dean Pospisil <dean@D-108-179-132-140.dhcp4.washington.edu>
2016-07-15 15:50:23 -0700

    small improvements to parse imagenet log

--- analysis/code/parse_image_net_log.py
+++ analysis/code/parse_image_net_log.py
@@ -14,47 +14,55 @@ top_dir = top_dir + 'v4cnn'
 sys.path.append(top_dir + 'common')
 sys.path.append(top_dir + 'img_gen')
 import xarray as xr
-f = open(top_dir + '/data/image_net/imagenet_log_May_21.txt', 'r')
+#f = open(top_dir + '/data/image_net/imagenet_log_May_21.txt', 'r')
 #f = open(top_dir + '/data/image_net/imagenet_log_training_June21_fromsnapshot.txt', 'r')
 #f = open(top_dir + '/data/image_net/imagenet_log_training_June19.txt', 'r')
 #f = open(top_dir + '/data/image_net/imagenet_log_training_June27.txt', 'r')
-f = open(top_dir + '/data/image_net/imagenet_log_training_July11th_faithful.txt', 'r')
+#f = open(top_dir + '/data/image_net/imagenet_log_training_July11th_faithful.txt', 'r')
+filenames = ['imagenet_log_training_july13th_rep_scrib_aws.txt',
+#'imagenet_log_training_july13th_rep_scrib_aws2.txt',
+#'imagenet_log_training_july13th_rep_scrib.txt',
+#'imagenet_log_training_july13th_rep_scrib2.txt',
+'imagenet_log_training_july13th_rep_scrib_aws2_long.txt',
+'imagenet_log_training_july14th_rep_scrib_batch100.txt',
+'imagenet_log_training_June14.txt',
+'imagenet_log_training_June19.txt',
+#'imagenet_log_training_June21_fromsnapshot.txt',
+'imagenet_log_training_June27.txt',
+'imagenet_log_May_21.txt']
 
-log = f.readlines()
+for filename in filenames:
+    f = open(top_dir + '/data/image_net/' + filename, 'r')
+
+    log = f.readlines()
 
 
-iter_loss = [re.findall('Iteration \d+.+loss = \d+.\d+', line) for line in log
-            if not re.findall('Iteration \d+.+loss = \d+.\d+', line)==[]]
-loss = np.array([np.double(re.split(' ',line[0])[-1]) for line in iter_loss])[:-1]
-iteration = np.array([np.double(re.split(' ', re.split(', ',line[0])[0])[1] ) for line in iter_loss])[:-1]
+    iter_loss = [re.findall('Iteration \d+.+loss = \d+.\d+', line) for line in log
+                if not re.findall('Iteration \d+.+loss = \d+.\d+', line)==[]]
+    loss = np.array([np.double(re.split(' ',line[0])[-1]) for line in iter_loss])[:-1]
+    iteration = np.array([np.double(re.split(' ', re.split(', ',line[0])[0])[1] ) for line in iter_loss])[:-1]
 
-lr = [re.findall(' lr = \d+.\d+', line) for line in log]
+    lr = [re.findall(' lr = \d+.\d+', line) for line in log]
 
-lr = np.array([float(re.split(' = ', line[0])[1]) for line in
-                [re.findall(' lr = \d+.\d+| lr = \de-\d+', line) for line in log]
-                if not line==[]])
+    lr = np.array([float(re.split(' = ', line[0])[1]) for line in
+                    [re.findall(' lr = \d+.\d+| lr = \de-\d+', line) for line in log]
+                    if not line==[]])
 
-acc = np.array([np.double(re.split(' = ', line[0])[1]) for line in
-                [re.findall('accuracy = \d+.\d+', line) for line in log]
-                if not line==[]])
-acc_iter = np.array([int(re.split(',', line[0])[0]) for line in
-                [re.findall('\d+, Testing net', line) for line in log]
-                if not line==[]])
-    
-    
-#acc = np.array([np.double(re.split(' ', line[0])[1]) for line in
-#                [re.findall('#0: \d+.\d+', line) for line in log]
-#                if not line==[]])
-#
-#    
-#plt.plot(iteration, loss)
-#plt.plot(iteration, lr)
-plt.plot(acc_iter, acc)
-#plt.xlabel('Iterations over batch of 256')
-#plt.ylabel('Performance')
-#plt.plot(range(0,len(acc)*1000,1000), acc)
+    acc = np.array([np.double(re.split(' = ', line[0])[1]) for line in
+                    [re.findall('accuracy = \d+.\d+', line) for line in log]
+                    if not line==[]])
+    acc_iter = np.array([int(re.split(',', line[0])[0]) for line in
+                    [re.findall('\d+, Testing net', line) for line in log]
+                    if not line==[]])
+    if len(acc_iter)>len(acc):
+        acc_iter = acc_iter[:len(acc)]
+    elif len(acc_iter)<len(acc):
+        acc = acc[:len(acc_iter)]
 
-plt.ylim(0,1)
+    plt.plot(acc_iter, acc)
+    plt.xlabel('Iterations')
+    plt.ylabel('Performance')
+    plt.ylim(0,1)
 
 
 f = open(top_dir + '/data/image_net/bvlc_reference_alexnet_train_log.txt', 'r')
@@ -64,3 +72,6 @@ acc = np.array([np.double(re.split(' ', line[0])[1]) for line in
                 if not line==[]])
 plt.plot(range(0,len(acc)*1000,1000), acc)
 
+#plt.legend(['aws1','aws2', 'scrib1', 'scrib2', 'aws60 long','scrib100 long','june 14 scrib82','june19th aws 227', 'june21 snap', 'may21_scrib', 'ref'])
+plt.legend(['aws1','aws60 long','scrib100 long','june 14 scrib82',
+'june19th aws 227', 'June 27 aws 256', 'may21_scrib', 'ref'], loc=0)
--- analysis/code/ref_cnn_v4_ti_apc_spar_analysis.py
+++ analysis/code/ref_cnn_v4_ti_apc_spar_analysis.py
@@ -57,18 +57,20 @@ def take_intersecting_1d_index(indexee, indexer):
 
     return xr.DataArray(np.squeeze(indexee.values), new_coords, new_dims)
 
-#def translation_invariance(da)      :
+
 
 da = v4_resp_ti
-da = alex_resp.load().squeeze()
-da = da.transpose('unit', 'x', 'shapes')
-da_ms = (da - da.mean(['shapes'])).squeeze()
-no_na = [unit.dropna('shapes', how='all').dropna('x', how='all') for unit in da_ms ]
-s = [np.linalg.svd(unit.transpose('shapes', 'x').values, compute_uv=0) for unit in no_na]
-best_ti = np.array([(asingval[0]**2)/(sum(asingval**2)) for asingval in s])
+def translation_invariance(da):
+    da = alex_resp.load().squeeze()
+    da = da.transpose('unit', 'x', 'shapes')
+    da_ms = (da - da.mean(['shapes'])).squeeze()
+    no_na = [unit.dropna('shapes', how='all').dropna('x', how='all') for unit in da_ms ]
+    s = [np.linalg.svd(unit.transpose('shapes', 'x').values, compute_uv=0) for unit in no_na]
+    best_ti = np.array([(asingval[0]**2)/(sum(asingval**2)) for asingval in s])
+    ti = xr.DataArray(np.squeeze(best_ti), dims='unit')
+    ti = take_intersecting_1d_index(ti, da)
 
-ti = xr.DataArray(np.squeeze(best_ti), dims='unit')
-ti = take_intersecting_1d_index(ti, da)
+    return ti
 
 #need to transmit all the meta-data, turn this into a definition.
 
