c3d045b8dc9b1536f87e79713d7b213e585c0b1a
Dean Pospisil <dean@D-108-179-132-140.dhcp4.washington.edu>
2016-07-25 12:23:55 -0700

    made simple net response with function to save jpegs with means subtracted BGR

--- nets/simple_net_response_img_dir.py
+++ nets/simple_net_response_img_dir.py
@@ -5,7 +5,23 @@ Created on Mon Jul 25 10:06:23 2016
 @author: dean
 """
 
-net = '/data/dean_data/net_stages/_ref_iter_0.caffemodel'
-stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(1000))
-da = cf.get_net_resp(base_image_nm, ann_dir, iter_name.split('stages/')[1].split('.')[0],
-                     stim_trans_cart_dict, stim_trans_dict, require_provenance=True)
+import caffe_net_response as cf
+ann_dir = '/data/dean_data/net_stages/'
+import os
+import sys
+top_dir = os.getcwd().split('v4cnn')[0]
+sys.path.append(top_dir+ 'v4cnn/')
+sys.path.append( top_dir + 'xarray')
+sys.path.append( top_dir + 'common')
+top_dir = top_dir + 'v4cnn/'
+
+response_file = top_dir + '/data/responses/nat_image_resp.nc'
+
+base_image_nm = 'natural'
+net = '/home/dean/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet'
+ann_dir = '/home/dean/caffe/models/bvlc_reference_caffenet/'
+stim_trans_cart_dict, stim_trans_dict = cf.stim_trans_generator(shapes=range(362))
+da = cf.get_net_resp(base_image_nm, ann_dir, net.split('stages/')[1].split('.')[0],
+                     stim_trans_cart_dict, stim_trans_dict, require_provenance=False)
+ds = da.to_dataset(name='resp')
+ds.to_netcdf(response_file)
