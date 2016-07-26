4ef8f5c34bbf1d22eac52f2b37d546d4028d7b22
Dean Pospisil <dean@D-108-179-132-140.dhcp4.washington.edu>
2016-07-25 18:14:13 -0700

    linear response plots

--- analysis/figures/scripts/nat_vs_artificial_image_resp.py
+++ analysis/figures/scripts/nat_vs_artificial_image_resp.py
@@ -36,9 +36,10 @@ def tick_format_d(x, pos):
             return(np.round(x,2))
 
 def nice_axes(axes, xticks=None, yticks=None, nxticks=5, nyticks=2):
-    
+
     for i, an_axes in enumerate(axes):
-        if i==len(axes)-1:
+#        if i==len(axes)-1:
+        if True:
             if yticks==None:
                 an_axes.yaxis.set_major_locator(mtick.LinearLocator(numticks=nyticks, presets=None))
             else:
@@ -49,11 +50,14 @@ def nice_axes(axes, xticks=None, yticks=None, nxticks=5, nyticks=2):
                 an_axes.set_xticks(xticks)
                 an_axes.xaxis.set_tick_params(length=0)
                 an_axes.yaxis.set_tick_params(length=0)
-                an_axes.yaxis.set_major_formatter(mtick.FuncFormatter(tick_format_d))
-            an_axes.xaxis.set_major_formatter(mtick.FuncFormatter(tick_format_d))
+                #an_axes.yaxis.set_major_formatter(mtick.FuncFormatter(tick_format_d))
+            #an_axes.xaxis.set_major_formatter(mtick.FuncFormatter(tick_format_d))
         else:
             an_axes.set_xticks([])
             an_axes.set_yticks([])
+
+
+
 def stacked_hist_layers(cnn, logx=False, logy=False, xlim=None, maxlim=False, bins=100):
     layers = cnn.index.get_level_values('layer_label').unique()
     if logx:
@@ -64,19 +68,19 @@ def stacked_hist_layers(cnn, logx=False, logy=False, xlim=None, maxlim=False, bi
         plt.subplot(len(layers), 1, i+1)
         vals = cnn.loc[layer].dropna().values.flatten()
 
-        
-        plt.hist(vals, log=logy, bins=bins, histtype='step', 
-                 range=xlim, normed=False)
-        
-        plt.plot([np.median(vals),]*2, np.array(plt.gca().get_ylim()), color='red')
+
+        plt.hist(vals, log=logy, bins=bins, histtype='step',
+                 range=xlim, normed=True)
+
+        #plt.plot([np.median(vals),]*2, np.array(plt.gca().get_ylim()), color='red')
         plt.gca().set_ylabel(layer, ha='right', rotation=0, labelpad=25)
         plt.gca().yaxis.set_label_position("right")
-    
+
     if logx:
         plt.xlabel('log')
-    nice_axes(plt.gcf().axes) 
+    nice_axes(plt.gcf().axes)
     #plt.suptitle(cnn.name)
-    
+
 
 #font = {'size' : 25}
 #mpl.rc('font', **font)
@@ -101,29 +105,40 @@ save_folder = top_dir + 'data/an_results/reference/'
 #ti_alex = pk.load(open(save_folder  + 'ti_'+ cnn_name, 'rb'))
 #tilc_alex = pk.load(open(save_folder  + 'trans_ill_cond_' + cnn_name, 'rb'))
 
-apc_resp = xr.open_dataset(top_dir + 'data/responses/' + cnn_name + '.nc')['resp'].load()
-apc_resp = apc_resp.sel(x=0).squeeze()
-nat_resp = xr.open_dataset(top_dir + 'data/responses/alex_net_nat_image_dist2000.nc')['r'].load()
-nat_resp = nat_resp
-nat_resp = nat_resp[:370,]
-apc =  apc_resp.reindex_like(apc_resp + nat_resp)
+if 'apc_resp' not in locals():
+    apc_resp = xr.open_dataset(top_dir + 'data/responses/' + cnn_name + '.nc')['resp'].load()
+    apc_resp = apc_resp.sel(x=0).squeeze()
+    nat_resp = xr.open_dataset(top_dir + 'data/responses/nat_image_resp.nc')['resp'].load()
+    nat_resp = nat_resp
+    nat_resp = nat_resp[:362,]
+    apc =  apc_resp.reindex_like(apc_resp + nat_resp)
 #nat_resp =  nat_resp.reindex_like(apc_resp + nat_resp)
 
 
 
-keys = [key for key in nat_resp['unit'].coords.keys()
-        if not nat_resp['unit'].coords[key].values.shape==() and key!='unit']
-keys = ['layer_label', 'layer_unit']
-coord = [nat_resp['unit'].coords[key].values
-        for key in keys]
-index = pd.MultiIndex.from_arrays(coord, names=keys)
-nat = pd.DataFrame(nat_resp.values.T, index=index)
-apc = pd.DataFrame(apc.values.T, index=index)
+    keys = [key for key in nat_resp['unit'].coords.keys()
+            if not nat_resp['unit'].coords[key].values.shape==() and key!='unit']
+    keys = ['layer_label', 'layer_unit']
+    coord = [nat_resp['unit'].coords[key].values
+            for key in keys]
+    index = pd.MultiIndex.from_arrays(coord, names=keys)
+    nat = pd.DataFrame(nat_resp.values.T, index=index)
+    nat = nat[(nat.index.get_level_values('layer_label') != 'conv4_conv4_0_split_0') ]
+    nat = nat[(nat.index.get_level_values('layer_label') != 'conv4_conv4_0_split_1') ]
+    keys = [key for key in apc['unit'].coords.keys()
+            if not apc['unit'].coords[key].values.shape==() and key!='unit']
+    keys = ['layer_label', 'layer_unit']
+    coord = [apc['unit'].coords[key].values
+            for key in keys]
+    index = pd.MultiIndex.from_arrays(coord, names=keys)
+    apc = pd.DataFrame(apc.values.T, index=index)
 
-stacked_hist_layers(nat, logx=False, logy=True, xlim=None, maxlim=False, bins=100)
-stacked_hist_layers(apc, logx=False, logy=True, xlim=None, maxlim=False, bins=100)
+plt.close('all')
+stacked_hist_layers(nat, logx=False, logy=False, xlim=None, maxlim=False, bins=100)
+stacked_hist_layers(apc, logx=False, logy=False, xlim=None, maxlim=False, bins=100)
 
-
+plt.legend(['nat', 'apc'])
+plt.subplots_adjust(left=.12, bottom=0.04, right=.9, top=.99, wspace=1, hspace=1)
 
 #
 #
@@ -133,30 +148,30 @@ stacked_hist_layers(apc, logx=False, logy=True, xlim=None, maxlim=False, bins=10
 #    eye_r2_v4 = pk.load(open(save_folder  + 'eye_r2_' + v4_name, 'rb'), encoding='latin1')
 #    eye_r2_alex = pk.load(open(save_folder  + 'eye_r2_' + cnn_name, 'rb'), encoding='latin1')
 #    k_alex = pk.load(open(save_folder  + 'k_' + cnn_name, 'rb'), encoding='latin1')
-#    
+#
 #    k_v4 = pk.load(open(save_folder  + 'k_' + v4_name, 'rb'), encoding='latin1')
 #    ti_v4 = pk.load(open(save_folder  + 'ti_'+ v4_name, 'rb'), encoding='latin1')
 #    ti_alex = pk.load(open(save_folder  + 'ti_'+ cnn_name, 'rb'), encoding='latin1')
 #    tilc_alex = pk.load(open(save_folder  + 'trans_ill_cond_' + cnn_name, 'rb'), encoding='latin1')
-#    
+#
 #    apc_alex = pk.load(open(save_folder  + 'apc_'+ cnn_name, 'rb'), encoding='latin1')
 #    apc_v4 = pk.load(open(save_folder  + 'apc_'+ v4_name, 'rb'), encoding='latin1')
-#    
+#
 #    ti_v4['layer_label'] = 'v4'
 #    ti_v4['layer_unit'] = range(len(ti_v4))
 #    k_v4['layer_unit'] = range(len(k_v4))
-#    
+#
 #    k_v4['layer_label'] = 'v4'
 #    ti_v4.set_index(['layer_label', 'layer_unit'], inplace=True)
-#    
+#
 #    apc_v4 = pd.concat( [apc_v4, coef_var_v4, eye_r2_v4, k_v4], axis=1).set_index(['layer_label', 'layer_unit'])
 #    cnn = pd.concat([coef_var_alex, eye_r2_alex, k_alex, ti_alex, tilc_alex, apc_alex], axis=1)
-#    
+#
 #    both=pd.concat([cnn, apc_v4, ti_v4])
 #    both = both[(both.index.get_level_values('layer_label') != 'prob')]
-#    
 #
-#    
+#
+#
 #    plt.close('all')
 #
-#     
+#
