66d56dbd16085fc71fdaad04c236be75258397da
Dean Pospisil <dean@sarus.biostr.washington.edu>
2017-04-21 15:32:38 -0700

    finished opponency, princomp map

--- analysis/figures/scripts/net_analysis_poster_vss.py
+++ analysis/figures/scripts/net_analysis_poster_vss.py
@@ -75,8 +75,9 @@ if 'netwts' not in locals() or goforit:
 netwts[5][1] = netwts[5][1].reshape((4096, 256, 6, 6))
 wts_by_layer = [layer[1] for layer in netwts]
 
-net_resp_name = 'bvlc_reference_caffenet_nat_image_resp_371.nc'
+net_resp_name = 'bvlc_reference_caffenety_test_APC362_pix_width[32.0]_x_(104.0, 124.0, 11)_x_(104.0, 124.0, 11)_amp_None.nc'
 da = xr.open_dataset(top_dir + '/data/responses/' + net_resp_name)['resp']
+da.coords['unit'] = range(da.shape[-1])
 #%%
 from more_itertools import unique_everseen
 layer_num = da.coords['layer']
@@ -84,12 +85,15 @@ layer_label_ind = da.coords['layer_label'].values
 split_layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5',]
 dims = ['unit','chan', 'y', 'x']
 layer_names = list(unique_everseen(layer_label_ind))
-layer_names = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6','fc7','fc8']
+layer_names = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6',]
 
 netwtsd = {}
-for layer, name in zip(wts_by_layer,layer_names):
+for layer, name in zip(wts_by_layer, layer_names):
     dim_names = dims[:len(layer.shape)]
-    netwtsd[name] = xr.DataArray(layer, dims=dim_names)
+    layer_ind = da.coords['layer_label'].values == name 
+    _ =  da[..., layer_ind].coords['unit']
+    netwtsd[name] = xr.DataArray(layer, dims=dims)
+    netwtsd[name].coords['unit'] = _
 
 
 conv1 = netwtsd['conv1']
@@ -103,18 +107,63 @@ plt.savefig(top_dir + '/analysis/figures/images/early_layer/1st_layer_filters.pd
 
 #%%
 def variance_to_power_ratio(da):
-    red_dims = list(set(da.dims) - set('unit'))
+    red_dims = list(set(da.dims) - set(['unit',]))
     var = ((da-da.mean('chan'))**2).sum(red_dims)
     pwr =  (da**2).sum(red_dims)
     return var/pwr
 
 def receptive_field(da):
-    return (da**2).sum(['x','y'])
+    return (da**2).sum('chan')
 
-def princ_comp_pwr_maps()
+def prin_comp_maps(da):
+    da = da.transpose('unit', 'chan', 'y', 'x')
+
+    data = da.values.reshape(da.shape[:2] + (np.product(da.shape[2:]),))
+    u, s, v = np.linalg.svd(data, full_matrices=False)
+    v = v.reshape(da.shape)
+    
+    u_da = xr.DataArray(u, dims=('unit', 'chan', 'pc'), 
+                        coords=[range(n) for n in np.shape(u)])
+    u_da.coords['unit'] = da.coords['unit']
+    s_da = xr.DataArray(s, dims=('unit', 'sv'), 
+                        coords=[range(n) for n in np.shape(s)])
+    s_da.coords['unit'] = da.coords['unit']
+    v_da = xr.DataArray(v, dims=('unit', 'pc', 'x', 'y'), 
+                        coords=[range(n) for n in np.shape(v)])
+    v_da.coords['unit'] = da.coords['unit']
+    
+    return u_da, s_da, v_da
+
+def spatial_opponency(da):
+    da = da.transpose('unit', 'chan', 'y', 'x')
+    data = da.values.reshape(da.shape[:2] + (np.product(da.shape[2:]),))
+    #print(data.shape)
+    cov = np.matmul(data.transpose(0, 2, 1), data)
+    #print(cov.shape)
+    cov = cov.sum(axis=(1,2)) - np.trace(cov, axis1=1, axis2=2)
+    #print(cov.shape)
+
+    
+    vnorm = np.linalg.norm(data, axis=1)
+    print(vnorm.shape)
+    outer_prod = vnorm*vnorm[:, np.newaxis, :]
+    print(outer_prod.shape)
+
+    outer_prod = outer_prod - np.trace(outer_prod)
+    opponency = np.sum(cov, (1, 2)) / np.sum(outer_prod, (1, 2))
+    
+    opponency_da = xr.DataArray(opponency, dims=('unit',))
+    opponency_da.coords['unit'] = da.coords['unit']
+    
+    return opponency_da
+
+da_ratio = variance_to_power_ratio(conv1)
+u_da, s_da, v_da = prin_comp_maps(conv1)
+rf = receptive_field(conv1)
+opponency_da = spatial_opponency(conv1)
+
     
     
-
 
     
     
