5d3fea64cac11dfd609cf0b8d7e43e9049dee697
dean <deanp3@uw.edu>
2017-06-08 13:26:47 -0700

    trying to get xarray working

--- analysis/figures/scripts/all_v4cnn_figures.py
+++ analysis/figures/scripts/all_v4cnn_figures.py
@@ -9,12 +9,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os, sys
 top_dir = os.getcwd().split('v4cnn')[0]
+#%%
 sys.path.append(top_dir+ 'v4cnn')
-sys.path.insert(0, top_dir + 'xarray');top_dir = top_dir + 'v4cnn/';
+sys.path.insert(0, top_dir + 'xarray/');
+top_dir = top_dir + 'v4cnn/';
 sys.path.append( top_dir + 'common')
 sys.path.append(top_dir +'/nets')
 import pickle as pk
-import xarray as xr;import pandas as pd
+import xarray as xr
+import pandas as pd
 import apc_model_fit as ac
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
