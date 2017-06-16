f034736a7ee9736ea02d0de93e2a1b243fdba77f
dean <deanp3@uw.edu>
2017-06-16 16:24:37 -0700

    ti for cadieu

--- analysis/code/ti_apc_for_cadieu.py
+++ analysis/code/ti_apc_for_cadieu.py
@@ -17,15 +17,24 @@ import d_misc as dm
 from scipy import io
 import matplotlib.pyplot as plt
 
+import d_net_analysis as na
+
 data = 'v4cnn/data/'
 dmod = xr.open_dataset(top_dir + data + 'models/apc_models_362_16X16.nc',
                        chunks = {'models':500, 'shapes': 370})['resp']
-da = io.loadmat(top_dir + data + 'responses/cadieu_resp.mat')['zzz']
-da = da.reshape(1,65,368)
-da = xr.DataArray(da, dims=['unit', 'x', 'shapes'])
-#cor = ac.cor_resp_to_model(da_c, dmod)
-rf = dn.in_rf(da, 32)
-rf[10:50] = 1
-ti = dn.ti_av_cov(da[:,12:30,:], rf=None)
-print(ti)
-plt.plot(da.sum('shapes').squeeze())
+da = io.loadmat(top_dir + data + 'responses/cadieu_109.mat')['all']
+resp = np.reshape(da, (65, 109, 368), order='F')
+da = xr.DataArray(resp, dims=['x', 'unit','shapes'], coords=[range(0, 65*4, 4), range(109),  range(368)])
+da = da.transpose('unit', 'x', 'shapes')
+ti =na.ti_in_rf(da, stim_width=64)
+#%%
+plt.hist(ti, bins=1000, histtype='step', cumulative=True)
+
+#da = da.reshape(1,65,368)
+#da = xr.DataArray(da, dims=['unit', 'x', 'shapes'])
+##cor = ac.cor_resp_to_model(da_c, dmod)
+#rf = dn.in_rf(da, 32)
+#rf[10:50] = 1
+#ti = dn.ti_av_cov(da[:,12:30,:], rf=None)
+#print(ti)
+#plt.plot(da.sum('shapes').squeeze())
